c31e6a3662a6171f1aff9dc571d9a2295b02dfb9
Andrii Reznik <icandrii.reznik@americanwell.com>
2022-09-01 00:58:48 +0200

    Added search dialog

---
+++ js/script.js
@@ -0,0 +1,19 @@
+$( function() {
+  $( "#dialog" ).dialog({
+    maxHeight: 50,
+    minHeight: 50,
+    autoOpen: false,
+    show: {
+      effect: "blind",
+      duration: 1000
+    },
+    hide: {
+      effect: "explode",
+      duration: 1000
+    }
+  });
+
+  $( "#opener" ).on( "click", function() {
+    $( "#dialog" ).dialog( "open" );
+  });
+} );
